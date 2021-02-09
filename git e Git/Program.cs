9bb18b3692f62eddd8babc7d1dfdb82d4f626d2c
Bruno Mimura <developer.bruno.mimur@gmail.com>
2021-02-09 17:39:49 -0300

    Adicionado Bom dia

--- git e Git/Program.cs
+++ git e Git/Program.cs
@@ -7,6 +7,7 @@ namespace git_e_Git
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia!");
         }
     }
 }
